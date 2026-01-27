760aba80fa9f024bea2f17d8e149fff3c6094630
wuyasan <youtosan0@gmail.com>
2026-01-27 00:59:51 +0000

    Making changes to the floorplan page UI, working well but some small fix ongoing.

--- src/components/mainMenu2.vue
+++ src/components/mainMenu2.vue
@@ -84,7 +84,7 @@
           </button>
 
           <div class="img-row">
-            <button class="img-btn img-btn--small" type="button" @click="openConductLink('general')">
+            <button class="img-btn img-btn--small" type="button" @click="setWindow('floorplan')">
               <img src="/public/FLOOR%20PLAN.png" alt="Floor Plan" />
             </button>
 
