7c6a382643c6767576a3956f2c233c19381b0531
wuyasan <youtosan0@gmail.com>
2026-01-28 21:44:51 +0000

    fixed small bug that mainmenu not closing after click floorplan

--- src/components/mainMenu2.vue
+++ src/components/mainMenu2.vue
@@ -44,7 +44,7 @@
           </button>
 
           <div class="img-row">
-            <button class="img-btn img-btn--small" type="button" @click="setWindow('floorplan')">
+            <button class="img-btn img-btn--small" type="button" @click="setWindowMobile('floorplan')">
               <img src="/public/FLOOR%20PLAN.png" alt="Floor Plan" />
             </button>
 
